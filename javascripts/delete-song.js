2504965f1bd0f4edd77940c4061daffd9a2dd832
Kaylee Cummings <kayleecummings@Kaylees-MacBook-Pro.local>
2016-03-08 09:53:42 -0600

    adding changes

--- javascripts/delete-song.js
+++ javascripts/delete-song.js
@@ -1,12 +1,12 @@
-"use strict";
+// "use strict";
 
 // Add delete button DOM to each row and, when it is clicked, delete the entire row in the DOM.
 
-var SongListBuilder = function(deleter) {
-  deleter.deleteSingleSong = function(id, text){
-    var divToRemove = document.getElementById(id);
-    divToRemove.parentNode.removeChild(divToRemove);
+// var SongListBuilder = function(deleter) {
+//   deleter.deleteSingleSong = function(id, text){
+//     var divToRemove = document.getElementById(id);
+//     divToRemove.parentNode.removeChild(divToRemove);
 
-  }
-  return deleter;
-}(SongListBuilder);
+//   }
+//   return deleter;
+// }(SongListBuilder);
